a4faaeba32024c675c08f0f465bd35b027bc3c71
alekSANdr <aleksandr@aleksandrs-imac.local>
2018-09-26 19:54:48 -0700

    got full response

--- JSONDownloadingSwift4/ViewController.swift
+++ JSONDownloadingSwift4/ViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     final let url = URL(string: "http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&tagged=ios&site=stackoverflow")
-
+    private var questions = [Question]()
+    @IBOutlet var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         downloadJson()
@@ -27,13 +30,26 @@ class ViewController: UIViewController {
             do
             {
                 let decoder = JSONDecoder()
-                let questions = try decoder.decode(Questions.self, from: data)
-                print(questions.items[0].title)
+                let downloadedQuestions = try decoder.decode(Questions.self, from: data)
+                self.questions = downloadedQuestions.items
+                self.tableView.reloadData()
             } catch {
                 print("something's wrong after downloading")
             }
             
         }.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionCell") as? QuestionCell else { return UITableViewCell() }
+        
+        cell.titleLabel.text = questions[indexPath.row].title
+        return cell
+        
     }
 
 }
